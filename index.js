6a64217637900930d60b0d0595c96df842221500
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-11-07 20:14:32 +0000

    purppeter

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require("express");
+const express = require('express');
+const puppeteer = require('puppeteer');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", async (req, res) => {
+  // Launch a headless browser
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
+  const url = "https://www.twitch.tv/directory";
+  await page.goto(url);
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  const title = await page.title();
+
+  await browser.close();
+
+  res.send(`Scraped Title: ${title}`);
 });
 
 app.listen(port, () => {
